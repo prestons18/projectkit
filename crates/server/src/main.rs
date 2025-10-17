71bf45a52fc21c19105d9378a2ad7aa11bcc2a9e
Preston Arnold <me@prestonarnold.uk>
2025-10-18 00:35:09 +0100

    Router & server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use api::router;
+use axum;
+
+#[tokio::main]
+async fn main() {
+    let app = router::router();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+    
+    println!("Listening on http://{}", addr);
+    axum::serve(listener, app).await.unwrap();
 }
