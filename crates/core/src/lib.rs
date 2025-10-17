f7b55fc21a387bffcb3f4b27aee168cdea43ecd3
Preston Arnold <me@prestonarnold.uk>
2025-10-18 00:07:24 +0100

    Initial commit - core crate

---
+++ crates/core/src/lib.rs
@@ -0,0 +1,13 @@
+pub use orm::error::{Error, Result};
+pub use orm::model::{FromRow, Model, ModelCrud, ModelQuery, Row, Value};
+pub use orm::backend::{Backend, DatabaseBackend};
+pub use orm::connection::{Connection, Database};
+pub use orm::query::{JoinType, OrderDirection, QueryBuilder};
+pub use orm::schema::{Column, Table};
+pub use orm::transaction::Transaction;
+pub mod orm_utils {
+    pub use orm::utils::{mysql_row_to_json, sqlite_row_to_json};
+}
+pub mod prelude {
+    pub use orm::prelude::*;
+}
